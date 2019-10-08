d8ef36a94db29385701e9f38578be249322ba152
Your Name <you@example.com>
2019-10-08 18:00:36 +0800

    0

--- homework.cpp
+++ homework.cpp
@@ -1,18 +1,19 @@
 //设计思路是先修改一下S，使得规则更为容易达到然后按照更为简单的规则判断
 //既然*号表示0个或多个，那么可以把所有*找出来之后，假设全部表示0个再把S中多余的字母抹掉
 //这样的话修改后的S与P规则修改为：*号表示复制前一个字母，“.”表示任意字母，P与S相同输出TRUE
-#include<iostream.h>
+#include<stdlib.h>
+#include<stdio.h>
 int isMatch(char * s,char * p) {//输入s和p若符合规则返回0否则返回1    (改变了p和s的值，因为不需要再输出p和s)
-char *pin_s = new char[100];//分配内存
-char *pin_p = new char[100];
+char *pin_s = (char *) malloc(1000 * sizeof(char));//分配内存
+char *pin_p = (char *) malloc(1000 * sizeof(char));
 pin_s = s;
 pin_p = p;
 int i_a;
-for(i_a = 0; i_a < 100; i_a++) {//此循环目的是搜索*
+for(i_a = 0; i_a < 1000; i_a++) {//此循环目的是搜索*
 switch(p[i_a]) {
 case '*' :
 int i_b;
-for(i_b = 97; i_b >= i_a-1; i_b--) {//此循环目的是将*号前边的字母插入S中
+for(i_b = 997; i_b >= i_a-1; i_b--) {//此循环目的是将*号前边的字母插入S中
 if(s[i_b] <= 'z' && s[i_b] >= 'a') {
 s[i_b+2]  = s[i_b]; 
 }
@@ -21,7 +22,7 @@ s[i_a-1]=p[i_a-1];
 s[i_a]=p[i_a-1];
 while(s[i_a] == s[i_a+1]) {//此循环目的是删除S中重复多余的*代表的字母
 int i_d;
-for(i_d = i_a; i_d < 100; i_d++) {
+for(i_d = i_a; i_d < 1000; i_d++) {
 s[i_d] = s[i_d+1];
 }
 }
@@ -30,7 +31,7 @@ break;
 }
 }
 int i;
-for(i = 0; i < 50; i++) {  //循环
+for(i = 0; i < 500; i++) {  //循环
 head:
 switch(p[i]) {
 case '.' :
@@ -49,14 +50,14 @@ break;
 return 1;
 }
 int main () {
-cout << " please input s and p " << endl;
-char s[100] = {0},p[100] = {0};
-cin >> s >> p ;
-int n;
+printf(" please input s and p ");
+char s[1000] = {0},p[1000] = {0};
+scanf("%s",&s);
+scanf("%s",&p);
 if(isMatch(s,p) == 0){
-cout << " false " << endl;
+printf(" false ");
 }else {
-cout << " true " << endl;
+printf(" true ");
 }
 return 0;
 }
